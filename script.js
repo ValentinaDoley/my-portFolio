8b4c7bab03c86c1604ef153b275838f640b9a3b3
ValentinaDoley <valentinadoley10@gmail.com>
2025-12-08 23:51:03 +0530

    Used GSAP features to add ScrollTrigger Plugin

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let menuIcon = docmunet.querySelector('.#menu-icon');
+// Setting up/activating the ScrollTrigger/turning on
+gsap.registerPlugin(ScrollTrigger);
+
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active');
 }
+
+// Animation Code setup
+//This finds ever <section> and animates it when you scroll
+const allSections = document.querySelectorAll("section");
+
+allSections.forEach((section) => {
+    gsap.from(section, {
+        y: 50,
+        opacity: 0,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: section,
+            start: "top 80%", // Animates when top of section hits 80% of viewport
+            toggleActions: "play reverse play reverse"
+        }
+    });
+});
